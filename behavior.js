b904b21902a832bbb18416aa0425f7259dd07b58
Jen <vobis.jennifer@yahoo.co.uk>
2019-05-10 13:50:54 +0100

    displaying first article on click, sorted out the listener so it corresponds with the section

--- behavior.js
+++ behavior.js
@@ -1,25 +1,27 @@
-$("select").on("click", function () {
+$("select").change(function () {
+  $(".articles").empty();
   const $section = $(this).val();
   const $article = $("<div></div>");
   $(".articles").append($article);
-  $article.addClass("article");
+  $article.addClass("article" + $section);
   const $p = $("<p></p>");
   const $h2 = $("<h2></h2>");
-  $(".article").append($h2);
-  $(".article").append($p);
-  $p.addClass("p");
-  $h2.addClass("h2");
+  $(".article" + $section).append($h2);
+  $(".article" + $section).append($p);
+  $p.addClass("p" + $section);
+  $h2.addClass("h2" + $section);
 
   $.ajax({
     method: 'GET',
-    url: 'https://api.nytimes.com/svc/topstories/v2/ ' + $section + ' .json?api-key=nyb43KA5zLUQ2199VSWc5ZPM7SiJPXrk'
+    url: 'https://api.nytimes.com/svc/topstories/v2/' + $section + '.json?api-key=nyb43KA5zLUQ2199VSWc5ZPM7SiJPXrk'
   })
     .done(function (data) {
-      $('.h2').append(data.results[0].title);
-      $('.p').append(data.results[0].abstract);
-      $('.article').css("background-image", "url(" + data.results[0].multimedia[4].url + ")");
+      $(".h2" + $section).append(data.results[0].title);
+      $(".p" + $section).append(data.results[0].abstract);
+      $('.article' + $section).css("background-image", "url(" + data.results[0].multimedia[4].url + ")");
     });
 });
+
 
 
 
